36012ef7636639b0be7de837becb37467c29966b
retr080s <retr080s@protonmail.com>
2022-10-25 22:57:54 +0200

    active player update/change

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -17,7 +19,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Roling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -33,9 +37,14 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
         current0El.textContent = currentScore; // CHANGE LATER
     } else {
         // Switch to next player
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 });
